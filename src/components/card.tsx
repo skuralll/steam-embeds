eeec71f848b5efe3812cd591a23058bd4920fff6
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-21 23:18:59 +0900

    タイトルの追加

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,16 +2,23 @@ import styles from "@/styles/card.module.css";
 import Image from "next/image";
 
 export default function Card() {
+  const title = "プレイ時間の長いゲーム";
+  const username = "skuralll";
+
   return (
     <div className={styles.card}>
       <div className={styles.header}>
         <Image
           src="https://avatars.akamai.steamstatic.com/a0dcc9416dc25a1cefd874a9b1698ec5301d5c78_full.jpg"
           alt="プロフィールアイコン"
-          width={184}
-          height={184}
+          width={152}
+          height={152}
           className={styles.user_icon}
         />
+        <div className={styles.card_info}>
+          <h1 className={styles.card_title}>{title}</h1>
+          <span>{username}</span>
+        </div>
       </div>
     </div>
   );
