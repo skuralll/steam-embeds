cec770873e454f71979883580c3e13ab3e9c356c
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-22 14:23:27 +0900

    フォントを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Card from "@/components/card";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
