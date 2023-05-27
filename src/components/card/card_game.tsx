c81a34984a82204473e77bc23ae9fb9e1b1453a1
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-28 00:09:35 +0900

    ストアページへのリンクを追加

--- src/components/card/card_game.tsx
+++ src/components/card/card_game.tsx
@@ -5,6 +5,7 @@ import { IconContext } from "react-icons";
 import { FiExternalLink } from "react-icons/fi";
 import { getGameHeaderURL, getGameSearchHeaderURL } from "@/lib/steamapi";
 import { getDateString } from "@/lib/utils";
+import Link from "next/link";
 
 // カードのヘッダ
 export default function CardGame({ game }: { game: PlayedGameData }) {
@@ -12,29 +13,34 @@ export default function CardGame({ game }: { game: PlayedGameData }) {
     game.rtime_last_played == 0 ? "" : getDateString(game.rtime_last_played);
 
   return (
-    <div className={styles.game_item}>
-      <div className={styles.game_icon}>
-        <Image
-          src={getGameSearchHeaderURL(game.appid)}
-          alt=""
-          width={120}
-          height={45.2}
-        />
+    <a
+      href={`https://store.steampowered.com/app/${game.appid}/`}
+      className={styles.link}
+    >
+      <div className={styles.game_item}>
+        <div className={styles.game_icon}>
+          <Image
+            src={getGameSearchHeaderURL(game.appid)}
+            alt=""
+            width={120}
+            height={45.2}
+          />
+        </div>
+        <div className={styles.info_stack}>
+          <div className={styles.info_first}>
+            <span className={styles.game_title}>{game.name}</span>
+          </div>
+          <div className={styles.info_second}>
+            <br></br>
+          </div>
+          <div className={styles.info_third}>
+            <span className={styles.last_play}>{lastPlay}</span>
+            <span className={styles.play_time}>
+              {Math.ceil((game.playtime_forever / 60) * 10) / 10 + " hours"}
+            </span>
+          </div>
+        </div>
       </div>
-      <div className={styles.info_stack}>
-        <div className={styles.info_first}>
-          <span className={styles.game_title}>{game.name}</span>
-        </div>
-        <div className={styles.info_second}>
-          <br></br>
-        </div>
-        <div className={styles.info_third}>
-          <span className={styles.last_play}>{lastPlay}</span>
-          <span className={styles.play_time}>
-            {Math.ceil((game.playtime_forever / 60) * 10) / 10 + " hours"}
-          </span>
-        </div>
-      </div>
-    </div>
+    </a>
   );
 }
